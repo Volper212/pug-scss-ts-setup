d01d7bf72b9c1d73e51070e082137396563f39c9
Volper212 <volpergaming@gmail.com>
2019-12-28 16:09:29 +0100

    :bug: Fix error when running `npm run instant`

--- src/setup.ts
+++ src/setup.ts
@@ -1,4 +1,4 @@
-import { emptyDir, copy, mkdirp } from 'fs-extra';
+import { emptyDir, copy, ensureDir } from 'fs-extra';
 
 import { log } from './logger';
 import { src, dist, srcFolders } from './paths';
@@ -7,10 +7,19 @@ import { copyForce } from './utility';
 export default async function setup(isExample = false): Promise<void> {
   log('Starting setup...');
 
+  async function setupSrc(): Promise<void> {
+    await copyForce(
+      `${__dirname}/../${isExample ? 'example' : 'default'}-src`,
+      src
+    );
+    await Promise.all(
+      Object.values(srcFolders).map(folder => ensureDir(folder))
+    );
+  }
+
   await Promise.all([
-    copyForce(`${__dirname}/../${isExample ? 'example' : 'default'}-src`, src), // Copy default src
+    setupSrc(),
     emptyDir(dist), // In case dist already existed; we don't want old files to take space on the server
-    Object.values(srcFolders).map(folder => mkdirp(folder)), // Create directories inside src
     copy(`${__dirname}/../default-config.js`, 'psst-config.js') // Copy default config file
   ]);
 
