826b6b24f2fedbfe1d59e22f736f159442cdf41d
Volper212 <volpergaming@gmail.com>
2020-01-26 21:43:55 +0100

    :ambulance: Fix nothing working without psst-setup.js

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,26 @@ import { MinifyOptions } from 'uglify-js';
 import { Options as PugOptions, LocalsObject } from 'pug';
 import { Options as TypescriptOptions } from 'rollup-typescript';
 
+export interface Config {
+  files: string[];
+  scripts: string[];
+  output?: string;
+  pugLocals?: PugOptions & LocalsObject;
+  uglifyOptions?: MinifyOptions;
+  typescriptOptions?: TypescriptOptions;
+}
+
+function getConfig(): Config {
+  try {
+    return require(`${process.cwd()}/psst-config.js`);
+  } catch {
+    return {
+      files: [],
+      scripts: []
+    };
+  }
+}
+
 export const {
   files,
   scripts,
@@ -9,11 +29,4 @@ export const {
   pugLocals,
   uglifyOptions,
   typescriptOptions
-} = require(`${process.cwd()}/psst-config.js`) as {
-  files: string[];
-  scripts: string[];
-  output?: string;
-  pugLocals?: PugOptions & LocalsObject;
-  uglifyOptions?: MinifyOptions;
-  typescriptOptions?: TypescriptOptions;
-};
+} = getConfig();
