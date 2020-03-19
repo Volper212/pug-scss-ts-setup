482191a94633f94abd4845a92724cc2f5700e86d
Volper212 <volpergaming@gmail.com>
2020-03-19 16:46:01 +0100

    :truck: Rename default-config.js to psst-config.js

--- src/premade.ts
+++ src/premade.ts
@@ -3,5 +3,5 @@ const premadeFolder = '../premade';
 export const premade = {
   defaultSrc: `${premadeFolder}/default-src`,
   exampleSrc: `${premadeFolder}/example-src`,
-  configFile: `${premadeFolder}/default-config.js`
+  configFile: `${premadeFolder}/psst-config.js`
 } as const;
